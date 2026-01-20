e7848bc7ef3c561db6b21ac2cf865fc6c2b69f72
manhphan <manphan341996@gmail.com>
2026-01-20 14:22:03 +0700

    update test

--- app/src/main/java/com/app/firebasedistribution/MainActivity.kt
+++ app/src/main/java/com/app/firebasedistribution/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.app.firebasedistribution
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        toast()
+
+    }
+
+    fun toast() {
+        Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT).show()
     }
 }
